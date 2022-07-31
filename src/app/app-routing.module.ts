28a988a15189506c6d11faf14b5c408460a98688
rvmodesto <raquel.modesto2@gmail.com>
2022-07-31 02:09:30 -0300

    Adicionando componentes + desenvolvendo background

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BackgroundComponent } from './background/background.component';
+import { ContatoComponent } from './contato/contato.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { SobreMimComponent } from './sobre-mim/sobre-mim.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', redirectTo: 'background', pathMatch: 'full'},
+  {path: 'menu', component: MenuComponent},
+  {path: 'contato', component:ContatoComponent},
+  {path: 'sobre-mim', component: SobreMimComponent},
+  {path: 'projetos', component: ProjetosComponent},
+  {path: 'background', component: BackgroundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SobreMimComponent } from './sobre-mim/sobre-mim.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { ContatoComponent } from './contato/contato.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    SobreMimComponent,
+    ProjetosComponent,
+    ContatoComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
